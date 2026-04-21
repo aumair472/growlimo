0e5d57b23f8014d33477bb9c0a54d429be77bcbc
Umair Ali <aumair472@gmail.com>
2026-04-22 02:45:13 +0500

    chore: cleanup repository and ignore build artifacts/secrets

--- src_legacy/data/services/index.js
+++
@@ -1,9 +0,0 @@
-import texasServices from './states/texas/index.js';
-import californiaServices from './states/california/index.js';
-
-const allServices = {
-  ...texasServices,
-  ...californiaServices
-};
-
-export default allServices;
